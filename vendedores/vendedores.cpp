3844348b71388ccf3ba9c3e133ee1cf2d6cacfb9
Ignacio Bergantinos <ignacio.bergantinos@morado.app>
2022-11-23 03:27:58 -0300

    create output stream, read data and send it to standard output

--- vendedores/vendedores.cpp
+++ vendedores/vendedores.cpp
@@ -21,6 +21,45 @@ int main(){
 
 	inputFileStream.close();
 
+
+
+	std::ofstream outputFileStream;
+	outputFileStream.open("totalSalesAmount.txt", std::ios::out);
+
+	std::array<std::string, MONTHS_IN_A_YEAR> months {
+		"Enero",
+		"Febrero",
+		"Marzo",
+		"Abril",
+		"Mayo",
+		"Junio",
+		"Julio",
+		"Agosto",
+		"Septiembre",
+		"Octumbre",
+		"Noviembre",
+		"Diciembre"
+ };
+
+	for(int region{}; region < NUMBER_OF_REGIONS; region++) {
+		std::cout << "---" << "REGION " << region << "---" << std::endl;
+
+		for(int seller{}; seller < NUMBER_OF_SELLERS; seller++) {
+			std::cout << seller;
+			std::cout << "VENDEDOR " << (seller + 1) << std::endl;
+
+			for(int month{}; month < MONTHS_IN_A_YEAR; month++)
+				std::cout << "ventas en " << months.at(month) << ": $" << totalSalesAmount.at(region).at(seller).at(month) << std::endl;
+		}
+
+	std::cout << std::endl;
+
+	} 
+
+	outputFileStream.write(reinterpret_cast<const char*>(&totalSalesAmount), sizeof (totalSalesAmount));
+
+  	outputFileStream.close();
+
 }
 
 // g++ vendedores.cpp -o output -std=c++2a
