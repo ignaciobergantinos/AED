0071bbe40fe2314629dd99bd69a3c67d0c5d75af
Ignacio Bergantinos <ignacio.bergantinos@morado.app>
2022-11-23 01:23:16 -0300

    Dim2: refactor Dim2.cpp

--- vendedores/dimension2/Dim2.cpp
+++ vendedores/dimension2/Dim2.cpp
@@ -2,9 +2,11 @@
 #include <array>
 #include <string>
 
+const int SELLERS_QUANTITY{3};
+
 int main(){
 
-    std::array<std::array<int,12>,3> totalSalesAmount{};
+    std::array<std::array<int,12>,SELLERS_QUANTITY> totalSalesAmount{};
 
     std::array<std::string, 12> months{
         "Enero",
@@ -26,7 +28,7 @@ int main(){
     for(int amountSold{}; std::cin >> amountSold >> month >> seller;)
         totalSalesAmount.at(seller - 1).at(month - 1) += amountSold;
 
-    for(int sellerNumber{}; sellerNumber < 3; sellerNumber++){
+    for(int sellerNumber{}; sellerNumber < SELLERS_QUANTITY; sellerNumber++){
         std::cout << "seller " << sellerNumber << ":" << std::endl;
 
         for(int monthNumber{}; monthNumber < 12; monthNumber++)
