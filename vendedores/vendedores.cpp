eabb998857282e3d628180198cb8c265cc901da4
Ignacio Bergantinos <ignacio.bergantinos@morado.app>
2022-11-23 02:55:35 -0300

    open tests file, get total sales amount and close file

--- vendedores/vendedores.cpp
+++ vendedores/vendedores.cpp
@@ -1,10 +1,23 @@
 #include <iostream>
 #include <array>
 #include <string>
+#include <fstream>
+
+
 
 int main(){
 
+	std::array<std::array<std::array<int,12>,3>,4> totalSalesAmount{};
 
+	std::ifstream inputFileStream;
+	inputFileStream.open("TestVendedores.txt");
+
+	int month, seller, region{};
+
+	for(int amountSold{}; inputFileStream >> amountSold >> month >> seller >> region;)
+		totalSalesAmount.at(region).at(seller - 1).at(month - 1) += amountSold;
+
+	inputFileStream.close();
 
 }
 
